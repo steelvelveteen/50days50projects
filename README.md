9771549dc256bf50073eae9cdf03059babaf0fab
steelvelveteen <joeyvico@gmail.com>
2020-12-06 19:15:11 +1030

    Completed a simple yet beautiful thing to learn mini project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 50days50projects
+
 A udemy course just for fun. 50 projects!!
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
 import './style.scss';
-const next = document.querySelector('#next');
+const progress = document.querySelector('#progress') as HTMLElement;
+const circles = document.querySelectorAll('.circle') as NodeListOf<Element>;
+const next = document.querySelector('#next') as HTMLButtonElement;
+const prev = document.querySelector('#prev') as HTMLButtonElement;
+let currentActive = 1;
+
 next?.addEventListener('click', () => {
-    console.log('Next button was clicked');
+    currentActive++;
+    if (currentActive > circles.length) currentActive = circles.length;
+    update();
 });
 
-const prev = document.querySelector('#prev');
 prev?.addEventListener('click', () => {
     console.log('Prev button was clicked');
+    currentActive--;
+    if (currentActive < 1) currentActive = 1;
+    update();
 });
+
+const update = () => {
+    circles.forEach((circle: Element, index: number) => {
+        if(index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll('.active') as NodeListOf<Element>;
+    progress.style.width = (((actives.length - 1) / (circles.length - 1)) * 100).toString() + '%';
+
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+
+
+}
